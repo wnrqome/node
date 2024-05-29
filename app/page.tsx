58e04bfc25431b4ac70ba2f5e573d6a43dfa8504
Patryk <119251@student.san.edu.pl>
2024-05-29 21:52:51 +0200

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-red-500">
+    <div className="text-white p-4 w-80 bg-clip-border p-6 bg-violet-600 border-4 border-violet-300 border-dashed">
       Patryk Grądzki 119251
     </div>
   );
